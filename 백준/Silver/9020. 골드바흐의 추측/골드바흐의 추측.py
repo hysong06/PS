0038f3f3a8feab2bfce136711fcd1a6aa8c8f8e4
Yonghwan Song <106457455+soyoha@users.noreply.github.com>
2022-11-15 08:11:52 +0900

    Update 골드바흐의 추측.py

--- 백준/Silver/9020. 골드바흐의 추측/골드바흐의 추측.py
+++ 백준/Silver/9020. 골드바흐의 추측/골드바흐의 추측.py
@@ -1,21 +1,19 @@
 import sys
 
+sieve = set(range(10001))
+for i in range(2, 100 + 1):
+    if i in sieve:
+        sieve -= set(range(i * i, 10000 + 1, i))
+
+# main
 input = sys.stdin.readline
 
-# make the sieve of Eratosthenes.
-sieve = [False, False] + [True] * (10000 - 1)
-for i in range(2, 100 + 1):
-    if sieve[i]:
-        for k in range(i * i, 10000 + 1, i):
-            sieve[k] = False
-
-# solution here.
 for _ in range(int(input())):
     n = int(input())
     left = right = n // 2
 
     while left > 1:
-        if sieve[left] and sieve[right]:
+        if left in sieve and right in sieve:
             print(left, right)
             break
         left -= 1
