4a14856a0c506a4c0d763b8c391f4df391dee48b
Yonghwan Song <106457455+soyoha@users.noreply.github.com>
2022-11-19 22:54:43 +0900

    [Silver IV] Title: 링, Time: 108 ms, Memory: 113112 KB -BaekjoonHub

--- 백준/Silver/3036. 링/링.py
+++ 백준/Silver/3036. 링/링.py
@@ -1,18 +1,10 @@
+import math
 import sys
 
 input = sys.stdin.readline
 N = int(input())
 rings = list(map(int, input().split()))
 
-
-def get_fraction(a, b):
-    A, B = a, b
-    for i in range(min(A, B), 1, -1):
-        if A % i == 0 and B % i == 0:
-            A //= i
-            B //= i
-    return f"{A}/{B}"
-
-
 for cur_ring in rings[1:]:
-    print(get_fraction(rings[0], cur_ring))
+    gcd = math.gcd(rings[0], cur_ring)
+    print(f"{rings[0] // gcd}/{cur_ring // gcd}")
