752808f76cd7ed776452c0a75fbdcaf594ccb5a0
Yonghwan Song <106457455+soyoha@users.noreply.github.com>
2022-11-26 15:25:15 +0900

    [Gold IV] Title: 서강그라운드, Time: 168 ms, Memory: 116452 KB -BaekjoonHub

---
+++ 백준/Gold/14938. 서강그라운드/서강그라운드.py
@@ -0,0 +1,31 @@
+import collections
+import heapq
+import sys
+
+input = sys.stdin.readline
+n, m, r = map(int, input().split())
+drops = [0] + list(map(int, input().split()))  # len(drops) == n
+graph = collections.defaultdict(list)
+for _ in range(r):
+    a, b, l = map(int, input().split())
+    graph[a].append((b, l))
+    graph[b].append((a, l))
+
+
+def dijkstra(start: int) -> int:
+    heap = [(0, start)]
+    dist = [float("inf")] * (n + 1)
+    dist[start] = 0
+
+    while heap:
+        path_len, node = heapq.heappop(heap)
+        for (nxt, length) in graph[node]:
+            alt = path_len + length
+            if alt <= dist[nxt] and alt <= m:
+                dist[nxt] = alt
+                heapq.heappush(heap, (alt, nxt))
+
+    return sum(drops[i] for i in range(1, n + 1) if dist[i] <= m)
+
+
+print(max(dijkstra(i) for i in range(1, n + 1)))
