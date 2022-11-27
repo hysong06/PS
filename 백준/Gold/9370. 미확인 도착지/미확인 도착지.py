cc3d17945d436f4113f697801785d05b9673b4ab
Yonghwan Song <106457455+soyoha@users.noreply.github.com>
2022-11-27 21:31:37 +0900

    [Gold II] Title: 미확인 도착지, Time: 388 ms, Memory: 126944 KB -BaekjoonHub

---
+++ 백준/Gold/9370. 미확인 도착지/미확인 도착지.py
@@ -0,0 +1,48 @@
+import collections
+import heapq
+import sys
+
+input = sys.stdin.readline
+
+for _ in range(int(input())):
+    # initialization
+    n, m, t = map(int, input().split())
+    s, g, h = map(int, input().split())
+    graph = collections.defaultdict(list)
+    for _ in range(m):
+        a, b, d = map(int, input().split())
+        graph[a].append((b, d))
+        graph[b].append((a, d))
+    candidates = (int(input()) for _ in range(t))
+
+    # solution
+    def dijkstra(start: int) -> list[int]:
+        heap = [(0, start)]
+        dist = [50_000_001] * (n + 1)  # max(m) * max(d) == 50_000_000
+        dist[start] = 0
+
+        while heap:
+            path_len, node = heapq.heappop(heap)
+            if dist[node] < path_len:
+                continue
+            for (link, length) in graph[node]:
+                alt = path_len + length
+                if alt < dist[link]:
+                    dist[link] = alt
+                    heapq.heappush(heap, (alt, link))
+
+        return dist
+
+    s_to = dijkstra(s)
+    g_to = dijkstra(g)
+    h_to = dijkstra(h)
+
+    path1, path2 = g_to[:], h_to[:]
+    for i in range(1, n + 1):
+        path1[i] += s_to[h] + h_to[g]
+        path2[i] += s_to[g] + g_to[h]
+
+    print(
+        *sorted(c for c in candidates if s_to[c] == path1[c] or s_to[c] == path2[c]),
+        sep="\n",
+    )
