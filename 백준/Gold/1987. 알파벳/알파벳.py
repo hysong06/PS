9c0d09fda2507d679b5f1f6374f83a20921629d0
Yonghwan Song <106457455+soyoha@users.noreply.github.com>
2022-11-28 07:20:59 +0900

    [Gold IV] Title: 알파벳, Time: 5712 ms, Memory: 175500 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.py
+++ 백준/Gold/1987. 알파벳/알파벳.py
@@ -2,26 +2,29 @@ import sys
 
 input = sys.stdin.readline
 R, C = map(int, input().split())
-graph = [list(map(str, input().rstrip())) for _ in range(R)]
-visit = [False] * 26
+board = [list(map(str, input().rstrip())) for _ in range(R)]
+
 answer = 1
-dy = [1, -1, 0, 0]
-dx = [0, 0, 1, -1]
+dr, dc = (1, -1, 0, 0), (0, 0, 1, -1)
+visit = [False] * 26
 
 
-def dfs(y: int, x: int, count) -> None:
+def dfs(r: int, c: int, depth: int) -> None:
     global answer
-    answer = max(answer, count)
+    answer = max(depth, answer)
 
     for i in range(4):
-        ny, nx = y + dy[i], x + dx[i]
-        if ny < 0 or ny >= R or nx < 0 or nx >= C or visit[ord(graph[ny][nx]) - 65]:
+        nr, nc = r + dr[i], c + dc[i]
+        if nr < 0 or nr >= R or nc < 0 or nc >= C:
             continue
-        visit[ord(graph[ny][nx]) - 65] = True
-        dfs(ny, nx, count + 1)
-        visit[ord(graph[ny][nx]) - 65] = False
+
+        idx = ord(board[nr][nc]) - 65  # if board[nr][nc] == "A", idx == 0
+        if not visit[idx]:
+            visit[idx] = True
+            dfs(nr, nc, depth + 1)
+            visit[idx] = False
 
 
-visit[ord(graph[0][0]) - 65] = True
+visit[ord(board[0][0]) - 65] = True
 dfs(0, 0, 1)
 print(answer)
