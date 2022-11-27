b5b88b93a8ffa46b29e9ef4d5c784e70cbf11526
Yonghwan Song <106457455+soyoha@users.noreply.github.com>
2022-11-28 07:29:52 +0900

    [Gold IV] Title: 알파벳, Time: 5544 ms, Memory: 173032 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.py
+++ 백준/Gold/1987. 알파벳/알파벳.py
@@ -2,7 +2,8 @@ import sys
 
 input = sys.stdin.readline
 R, C = map(int, input().split())
-board = [list(map(str, input().rstrip())) for _ in range(R)]
+board = [list(map(lambda x: ord(x) - 65, input().rstrip())) for _ in range(R)]
+# each alphabet is replaced with its order. (A-Z == 0-25)
 
 answer = 1
 dr, dc = (1, -1, 0, 0), (0, 0, 1, -1)
@@ -15,16 +16,13 @@ def dfs(r: int, c: int, depth: int) -> None:
 
     for i in range(4):
         nr, nc = r + dr[i], c + dc[i]
-        if nr < 0 or nr >= R or nc < 0 or nc >= C:
+        if nr < 0 or nr >= R or nc < 0 or nc >= C or visit[board[nr][nc]]:
             continue
-
-        idx = ord(board[nr][nc]) - 65  # if board[nr][nc] == "A", idx == 0
-        if not visit[idx]:
-            visit[idx] = True
-            dfs(nr, nc, depth + 1)
-            visit[idx] = False
+        visit[board[nr][nc]] = True
+        dfs(nr, nc, depth + 1)
+        visit[board[nr][nc]] = False
 
 
-visit[ord(board[0][0]) - 65] = True
+visit[board[0][0]] = True
 dfs(0, 0, 1)
 print(answer)
