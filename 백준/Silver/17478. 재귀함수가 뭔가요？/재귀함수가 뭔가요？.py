02004d777b2417c44ca792c217df72cd3ddf6dfd
Yonghwan Song <106457455+soyoha@users.noreply.github.com>
2022-11-29 17:08:54 +0900

    [Silver V] Title: 재귀함수가 뭔가요?, Time: 116 ms, Memory: 113112 KB -BaekjoonHub

--- 백준/Silver/17478. 재귀함수가 뭔가요？/재귀함수가 뭔가요？.py
+++ 백준/Silver/17478. 재귀함수가 뭔가요？/재귀함수가 뭔가요？.py
@@ -7,10 +7,9 @@ N = int(input())
 def recursion(depth: int) -> None:
     if depth > N:
         return
-
     head = "____" * depth
+    
     print(head + "\"재귀함수가 뭔가요?\"")
-
     if depth == N:
         print(head + "\"재귀함수는 자기 자신을 호출하는 함수라네\"")
     else:
@@ -18,7 +17,6 @@ def recursion(depth: int) -> None:
         print(head + "마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.")
         print(head + "그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"")
         recursion(depth + 1)
-    
     print(head + "라고 답변하였지.")
 
 
