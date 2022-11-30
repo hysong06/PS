2a2ab2274616cb26e1f28d9c99e518db92980921
Yonghwan Song <106457455+soyoha@users.noreply.github.com>
2022-11-30 14:34:05 +0900

    [Bronze II] Title: 슈퍼 마리오, Time: 108 ms, Memory: 113112 KB -BaekjoonHub

--- 백준/Bronze/2851. 슈퍼 마리오/슈퍼 마리오.py
+++ 백준/Bronze/2851. 슈퍼 마리오/슈퍼 마리오.py
@@ -6,7 +6,7 @@ scores = [int(input()) for _ in range(10)]
 for i in range(1, 10):
     scores[i] += scores[i - 1]
 
-idx = min(bisect.bisect_left(scores, 100), 9)
+idx = max(1, min(bisect.bisect_left(scores, 100), 9))
 print(
     scores[idx - 1]
     if abs(scores[idx - 1] - 100) < abs(scores[idx] - 100)
