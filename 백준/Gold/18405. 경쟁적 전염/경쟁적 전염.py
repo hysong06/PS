46042c887c6408d3bde6a5913a693d374c061268
Yonghwan Song <106457455+soyoha@users.noreply.github.com>
2022-12-02 07:42:35 +0900

    [Gold V] Title: 경쟁적 전염, Time: 240 ms, Memory: 120052 KB -BaekjoonHub

--- 백준/Gold/18405. 경쟁적 전염/경쟁적 전염.py
+++ 백준/Gold/18405. 경쟁적 전염/경쟁적 전염.py
@@ -1,3 +1,4 @@
+import collections
 import heapq
 import sys
 
@@ -7,15 +8,16 @@ types = [list(map(int, input().split())) for _ in range(N)]
 S, X, Y = map(int, input().split())
 
 dr, dc = (1, -1, 0, 0), (0, 0, 1, -1)
-heap = []
-for i in range(N):
-    for j in range(N):
-        if types[i][j] == 0:
-            continue
-        heapq.heappush(heap, (0, types[i][j], i, j))
-
+queue = collections.deque()
+heap = [(types[i][j], i, j) for i in range(N) for j in range(N) if types[i][j] != 0]
+heapq.heapify(heap)
 while heap:
-    time, virus, r, c = heapq.heappop(heap)
+    _, i, j = heapq.heappop(heap)
+    queue.append((i, j, 0))
+
+# bfs
+while queue:
+    r, c, time = queue.popleft()
     if time == S:
         continue
     for i in range(4):
@@ -23,7 +25,7 @@ while heap:
         if nr < 0 or nr >= N or nc < 0 or nc >= N or types[nr][nc] > 0:
             continue
         # virus spreads.
-        types[nr][nc] = virus
-        heapq.heappush(heap, (time + 1, virus, nr, nc))
+        types[nr][nc] = types[r][c]
+        queue.append((nr, nc, time + 1))
 
 print(types[X - 1][Y - 1])
