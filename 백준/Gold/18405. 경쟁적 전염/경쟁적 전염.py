911f54023982b50d23d84b831af560f73722fe49
Yonghwan Song <106457455+soyoha@users.noreply.github.com>
2022-12-02 08:07:55 +0900

    [Gold V] Title: 경쟁적 전염, Time: 240 ms, Memory: 120052 KB -BaekjoonHub

--- 백준/Gold/18405. 경쟁적 전염/경쟁적 전염.py
+++ 백준/Gold/18405. 경쟁적 전염/경쟁적 전염.py
@@ -9,6 +9,9 @@ S, X, Y = map(int, input().split())
 
 dr, dc = (1, -1, 0, 0), (0, 0, 1, -1)
 queue = collections.deque()
+
+# if virus exsits in (i, j),
+# push (i, j, 0) into queue in the virus order by using heap.
 heap = [(types[i][j], i, j) for i in range(N) for j in range(N) if types[i][j] != 0]
 heapq.heapify(heap)
 while heap:
@@ -22,10 +25,11 @@ while queue:
         continue
     for i in range(4):
         nr, nc = r + dr[i], c + dc[i]
-        if nr < 0 or nr >= N or nc < 0 or nc >= N or types[nr][nc] > 0:
+        if nr < 0 or nr >= N or nc < 0 or nc >= N or types[nr][nc] != 0:
             continue
         # virus spreads.
-        types[nr][nc] = types[r][c]
-        queue.append((nr, nc, time + 1))
+        if time < S:
+            types[nr][nc] = types[r][c]
+            queue.append((nr, nc, time + 1))
 
 print(types[X - 1][Y - 1])
