08fbdd628979576e7217afcd038d76fb188f573c
Yonghwan Song <106457455+soyoha@users.noreply.github.com>
2022-12-06 18:29:01 +0900

    [Silver I] Title: 스타트링크, Time: 240 ms, Memory: 137308 KB -BaekjoonHub

--- 백준/Silver/5014. 스타트링크/스타트링크.py
+++ 백준/Silver/5014. 스타트링크/스타트링크.py
@@ -3,24 +3,19 @@ import sys
 
 input = sys.stdin.readline
 F, S, G, U, D = map(int, input().split())
+queue = collections.deque([(S, 0)])
+visit = [False] * (F + 1)
 
-
-def bfs() -> None:
-    queue = collections.deque([(S, 0)])
-    visit = [False] * (F + 1)
-
-    while queue:
-        floor, depth = queue.popleft()
-        if floor < 1 or floor > F or visit[floor]:
-            continue
-        if floor == G:
-            print(depth)  # if possible
-            return
-        visit[floor] = True
-        queue.append((floor + U, depth + 1))
-        queue.append((floor - D, depth + 1))
-
+# bfs
+while queue:
+    floor, depth = queue.popleft()
+    if floor < 1 or floor > F or visit[floor]:
+        continue
+    if floor == G:
+        print(depth)  # if possible
+        break
+    visit[floor] = True
+    queue.append((floor + U, depth + 1))
+    queue.append((floor - D, depth + 1))
+else:
     print("use the stairs")  # if impossible
-
-
-bfs()
