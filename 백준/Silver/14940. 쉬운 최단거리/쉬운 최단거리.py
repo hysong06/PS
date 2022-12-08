a689dec09cf09774c44f084eac94fb054d20ffc6
Yonghwan Song <106457455+soyoha@users.noreply.github.com>
2022-12-08 23:54:33 +0900

    [Silver I] Title: 쉬운 최단거리, Time: 268 ms, Memory: 123340 KB -BaekjoonHub

--- 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.py
+++ 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.py
@@ -12,22 +12,20 @@ dr, dc = (1, -1, 0, 0), (0, 0, 1, -1)
 
 for i in range(n):
     for j in range(m):
-        if ground[i][j] == 0:
+        if ground[i][j] == 0:  # process the unreachable points.
             dist[i][j] = 0
-        elif ground[i][j] == 2:
-            queue.append((i, j, 0))
+        elif ground[i][j] == 2:  # process the start point.
+            queue.append((i, j))
+            dist[i][j] = 0
 
 while queue:
-    r, c, depth = queue.popleft()
-    if dist[r][c] != -1:
-        continue
-    dist[r][c] = depth
-
+    r, c = queue.popleft()
     for i in range(4):
         nr, nc = r + dr[i], c + dc[i]
-        if nr < 0 or nr >= n or nc < 0 or nc >= m:
+        if nr < 0 or nr >= n or nc < 0 or nc >= m or dist[nr][nc] != -1:
             continue
-        queue.append((nr, nc, depth + 1))
+        queue.append((nr, nc))
+        dist[nr][nc] = dist[r][c] + 1
 
 
 for row in dist:
