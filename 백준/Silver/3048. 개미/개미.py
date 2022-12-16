daddbf6b951e0173dd12e4ff2fc2ce41abb11f78
YongHwan Song <106457455+yonghwna@users.noreply.github.com>
2022-12-16 20:46:57 +0900

    [Silver IV] Title: 개미, Time: 108 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Silver/3048. 개미/개미.py
@@ -0,0 +1,19 @@
+import sys
+
+input = sys.stdin.readline
+N1, N2 = map(int, input().split())
+group1 = input().rstrip("\n")[::-1]
+group2 = input().rstrip("\n")
+T = int(input())
+order = list(group1 + group2)
+
+# solution
+for _ in range(T):
+    i = 0
+    while i < len(order) - 1:
+        if order[i] in group1 and order[i + 1] in group2:
+            order[i], order[i + 1] = order[i + 1], order[i]
+            i += 1
+        i += 1
+
+print("".join(order))
