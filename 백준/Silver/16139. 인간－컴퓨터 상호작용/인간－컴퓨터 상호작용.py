e9d73e3cfa19263979158eb10d8161502be3286b
YongHwan Song <106457455+yonghwna@users.noreply.github.com>
2022-12-23 11:48:27 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 420 ms, Memory: 160256 KB -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
@@ -1,19 +1,21 @@
+import collections
 import sys
 
 input = sys.stdin.readline
-S = input().rstrip()
+S = input().rstrip("\n")
 q = int(input())
 
 # counts[-1] is always 0 to prevent wrong indexing.
-counts = {alpha: [0] * (len(S) + 1) for alpha in "abcdefghijklmnopqrstuvwxyz"}
+counts = collections.defaultdict(lambda: [0] * (len(S) + 1))
 for i, alpha in enumerate(S):
     counts[alpha][i] += 1
 
-for alpha in counts.keys():
-    # get the prefix-sum list of each alphabet.
+for count in counts.values():
+    # get the prefix-sum list of each alphabet in S.
     for i in range(1, len(S)):
-        counts[alpha][i] += counts[alpha][i - 1]
+        count[i] += count[i - 1]
 
 for _ in range(q):
     alpha, l, r = input().split()
-    print(counts[alpha][int(r)] - counts[alpha][int(l) - 1])
+    l, r = int(l), int(r)
+    print(counts[alpha][r] - counts[alpha][l - 1])
