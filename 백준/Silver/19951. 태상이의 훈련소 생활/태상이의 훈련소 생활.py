162baf154463c954db9ca0a4847f655b1fe343b2
YongHwan Song <106457455+yonghwna@users.noreply.github.com>
2022-12-23 10:34:09 +0900

    [Silver I] Title: 태상이의 훈련소 생활, Time: 236 ms, Memory: 128916 KB -BaekjoonHub

--- 백준/Silver/19951. 태상이의 훈련소 생활/태상이의 훈련소 생활.py
+++ 백준/Silver/19951. 태상이의 훈련소 생활/태상이의 훈련소 생활.py
@@ -4,13 +4,15 @@ input = sys.stdin.readline
 N, M = map(int, input().split())
 heights = list(map(int, input().split()))
 
+# don't need variance[N].
+# variance[N] is just for preventing index error.
 variance = [0] * (N + 1)
 for _ in range(M):
     a, b, k = map(int, input().split())
     variance[a - 1] += k
     variance[b] -= k
 
-for i in range(1, len(variance)):
-    variance[i] += variance[i - 1]  # prefix-sum
+for i in range(1, N):
+    variance[i] += variance[i - 1]  # prefix sum
 
-print(*map(sum, zip(heights, variance[:-1])))
+print(*map(sum, zip(heights, variance)))
