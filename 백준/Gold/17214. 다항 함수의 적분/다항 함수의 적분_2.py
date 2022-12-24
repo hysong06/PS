d38896620ada172c174df8d300d2220c50033a69
YongHwan Song <106457455+yonghwna@users.noreply.github.com>
2022-12-25 08:06:16 +0900

    [Gold V] Title: 다항 함수의 적분, Time: 188 ms, Memory: 119472 KB -BaekjoonHub

---
+++ 백준/Gold/17214. 다항 함수의 적분/다항 함수의 적분_2.py
@@ -0,0 +1,30 @@
+import re
+import sys
+
+
+def solution(expr: str) -> str:
+    if expr == "0":
+        return "W"
+
+    result = ""
+    if expr[0] == "-":
+        result += expr[0]
+        expr = expr[1:]
+
+    terms = re.split("[+-]", expr)
+    operators = [e for e in expr if e == "+" or e == "-"]
+
+    def integral(term: str) -> str:
+        if term.isdigit():
+            return ("" if term == "1" else term) + "x"
+        else:
+            c = term[:-1]  # coefficient
+            return ("" if c == "2" else str(int(c) // 2)) + "xx"
+
+    for term, oper in zip(terms[:-1], operators):
+        result += integral(term)
+        result += oper
+    return result + integral(terms[-1]) + "+W"
+
+
+print(solution(sys.stdin.readline().rstrip("\n")))
