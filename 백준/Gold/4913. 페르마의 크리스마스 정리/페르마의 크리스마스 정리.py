376a53b096ea3c7df74bfac0acfecf35bf6ea448
YongHwan Song <106457455+yonghwna@users.noreply.github.com>
2022-12-25 05:43:30 +0900

    [Gold IV] Title: 페르마의 크리스마스 정리, Time: 200 ms, Memory: 138984 KB -BaekjoonHub

--- 백준/Gold/4913. 페르마의 크리스마스 정리/페르마의 크리스마스 정리.py
+++ 백준/Gold/4913. 페르마의 크리스마스 정리/페르마의 크리스마스 정리.py
@@ -1,13 +1,15 @@
 import sys
 
+max_U = 1_000_000  # not a prime number
+
 """
 implement the sieve of Eratosthenes.
 """
-is_prime = [True] * 1_000_001
+is_prime = [True] * (max_U + 1)
 is_prime[0] = is_prime[1] = False
-for i in range(2, 1001):
+for i in range(2, int(len(is_prime) ** 0.5)):
     if is_prime[i]:
-        for k in range(i * i, 1_000_001, i):
+        for k in range(i * i, len(is_prime), i):
             is_prime[k] = False
 
 """
@@ -15,15 +17,14 @@ implement prefix-sum lists.
 1) 'targets' are primes that is expressible as a**2 + b**2.
 2) count_primes[0] and count_targets[0] is used as a border to negatives.
 """
-count_primes = [0] * 1_000_001
-count_targets = [0] * 1_000_001
+count_primes = [0] * (max_U + 1)
+count_targets = [0] * (max_U + 1)
 
 # 2 is a 'target' because 2 == 1**1 + 1**1.
 count_primes[2] = count_targets[2] = 1
-for i in range(3, 1_000_000):
+for i in range(3, max_U + 1):
     count_primes[i] = count_primes[i - 1] + int(is_prime[i])
-    count_targets[i] = count_targets[i - 1] + int(is_prime[i] and i % 4 == 1)
-
+    count_targets[i] = count_targets[i - 1] + (1 if is_prime[i] and i % 4 == 1 else 0)
 
 # main
 input = sys.stdin.readline
