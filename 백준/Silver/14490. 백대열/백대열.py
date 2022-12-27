2b70416255de38787910296e506f2d0de881f518
YongHwan Song <106457455+yonghwna@users.noreply.github.com>
2022-12-27 09:35:25 +0900

    [Silver V] Title: 백대열, Time: 140 ms, Memory: 113112 KB -BaekjoonHub

--- 백준/Silver/14490. 백대열/백대열.py
+++ 백준/Silver/14490. 백대열/백대열.py
@@ -1,11 +1,5 @@
-import sys
+import math
 
-input = sys.stdin.readline
 n, m = map(int, input().split(":"))
-
-for i in range(min(n, m) + 1, 1, -1):
-    if n % i == m % i == 0:
-        n //= i
-        m //= i
-
-print(f"{n}:{m}")
+k = math.gcd(n, m)
+print(f"{n // k}:{m // k}")
