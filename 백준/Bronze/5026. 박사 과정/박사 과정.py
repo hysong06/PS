22b5f3548208c5c65bd552cc2697ce05ed89e6ee
YongHwan Song <106457455+yonghwna@users.noreply.github.com>
2022-12-27 11:52:54 +0900

    [Bronze II] Title: 박사 과정, Time: 124 ms, Memory: 114328 KB -BaekjoonHub

--- 백준/Bronze/5026. 박사 과정/박사 과정.py
+++ 백준/Bronze/5026. 박사 과정/박사 과정.py
@@ -3,4 +3,4 @@ import sys
 input = sys.stdin.readline
 for _ in range(int(input())):
     problem = input().rstrip()
-    print("skipped" if problem == "P=NP" else eval(problem))
+    print("skipped" if problem == "P=NP" else sum(map(int, problem.split("+"))))
