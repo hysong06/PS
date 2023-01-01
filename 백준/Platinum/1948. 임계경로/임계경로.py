b9d20c2b65cb38375059e8d3ec53f5fa0642a408
YongHwan Song <106457455+yonghwna@users.noreply.github.com>
2023-01-01 23:25:24 +0900

    [Platinum V] Title: 임계경로, Time: 224 ms, Memory: 123880 KB -BaekjoonHub

--- 백준/Platinum/1948. 임계경로/임계경로.py
+++ 백준/Platinum/1948. 임계경로/임계경로.py
@@ -6,49 +6,60 @@ input = sys.stdin.readline
 n = int(input())
 m = int(input())
 graph = collections.defaultdict(dict)
-prevs = collections.defaultdict(list)
 indegree = [0] * (n + 1)
 for _ in range(m):
     a, b, c = map(int, input().split())
     graph[a][b] = c
-    prevs[b].append(a)
     indegree[b] += 1
 start, end = map(int, input().split())
-queue = collections.deque()
 
 
 """ topology sort """
+queue = collections.deque()
 queue.append(start)
-start_to = [0] * (n + 1)
+order = []
 
 while queue:
     cur = queue.popleft()
-    for (nxt, cur_to_nxt) in graph[cur].items():
-        start_to[nxt] = max(start_to[nxt], start_to[cur] + cur_to_nxt)
+    order.append(cur)
+    for (nxt, _) in graph[cur].items():
         indegree[nxt] -= 1
         if indegree[nxt] == 0:
             queue.append(nxt)
 
-max_len = start_to[end]
+# queue is empty.
+
+
+""" dp[i] == the longest path from start to i. """
+""" prevs can get the longest path from end to start. """
+dp = [0] * (n + 1)
+prevs = collections.defaultdict(list)
+
+for cur in order:
+    for (nxt, weight) in graph[cur].items():
+        path_len = dp[cur] + weight
+        if path_len > dp[nxt]:
+            dp[nxt] = path_len
+            prevs[nxt] = [cur]
+        elif path_len == dp[nxt]:
+            prevs[nxt].append(cur)
 
 
 """ count the edges in the longest path(s). """
-queue.append((end, 0))
+queue.append(end)
 visit = [False] * (n + 1)
 visit[end] = True
 counts = 0
 
 while queue:
-    cur, cur_to_end = queue.popleft()
+    cur = queue.popleft()
     for prev in prevs[cur]:
-        prev_to_end = graph[prev][cur] + cur_to_end
-        if start_to[prev] + prev_to_end == max_len:
-            counts += 1
-            if not visit[prev]:
-                visit[prev] = True
-                queue.append((prev, prev_to_end))
+        counts += 1
+        if not visit[prev]:
+            visit[prev] = True
+            queue.append(prev)
 
 
 """ answers """
-print(max_len)
+print(dp[end])
 print(counts)
