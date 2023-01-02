54695224edb9f4921843e794e2d6eb1255f60f0a
YongHwan Song <106457455+yonghwna@users.noreply.github.com>
2023-01-02 21:09:19 +0900

    [Silver V] Title: 단어 나누기, Time: 112 ms, Memory: 114488 KB -BaekjoonHub

--- 백준/Silver/1251. 단어 나누기/단어 나누기.py
+++ 백준/Silver/1251. 단어 나누기/단어 나누기.py
@@ -1,12 +1,11 @@
 import sys
 
 input = sys.stdin.readline
-word = input().rstrip()
-results = []
-
-for i in range(1, len(word) - 1):
-    for j in range(i + 1, len(word)):
-        a, b, c = word[:i], word[i:j], word[j:]
-        results.append(a[::-1] + b[::-1] + c[::-1])
-
-print(sorted(results)[0])
+word = input().rstrip("\n")
+print(
+    sorted(
+        word[:i][::-1] + word[i:j][::-1] + word[j:][::-1]
+        for i in range(1, len(word) - 1)
+        for j in range(i + 1, len(word))
+    )[0]
+)
