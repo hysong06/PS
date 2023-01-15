4fcca1e79ced626258b0b9814bf5cd9eaf605213
YongHwan Song <106457455+yonghwna@users.noreply.github.com>
2023-01-15 23:40:58 +0900

    [Silver III] Title: Four Squares, Time: 40 ms, Memory: 32540 KB -BaekjoonHub

--- 백준/Silver/17626. Four Squares/Four Squares.py
+++ 백준/Silver/17626. Four Squares/Four Squares.py
@@ -1,21 +1,23 @@
-def solution(n: int) -> int:
-    sqrt_n = int(n**0.5)
+import math
+import sys
 
-    if n == sqrt_n**2:
+
+def solution(n: int) -> int:
+    if math.sqrt(n).is_integer():
         return 1
 
-    for a in range(1, sqrt_n + 1):
-        b = int((n - a**2) ** 0.5)
-        if n == a**2 + b**2:
+    squares = {i**2 for i in range(1, int(math.sqrt(n)) + 1)}
+
+    for a in squares:
+        if n - a in squares:
             return 2
 
-    for a in range(1, sqrt_n + 1):
-        for b in range(a, int((n - a**2) ** 0.5) + 1):
-            c = int((n - a**2 - b**2) ** 0.5)
-            if n == a**2 + b**2 + c**2:
+    for a in squares:
+        for b in squares:
+            if n - a - b in squares:
                 return 3
 
     return 4
 
 
-print(solution(int(input())))
+print(solution(int(sys.stdin.readline())))
