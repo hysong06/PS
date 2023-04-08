dd211d7ff18cec9cddcbe94374127db38a6606ac
YongHwan Song <106457455+yonghwna@users.noreply.github.com>
2023-04-09 07:21:24 +0900

    [Silver IV] Title: 점프왕 쩰리 (Small), Time: 144 ms, Memory: 114436 KB -BaekjoonHub

--- 백준/Silver/16173. 점프왕 쩰리 （Small）/점프왕 쩰리 （Small）.py
+++ 백준/Silver/16173. 점프왕 쩰리 （Small）/점프왕 쩰리 （Small）.py
@@ -7,21 +7,20 @@ board = [list(map(int, input().split())) for _ in range(N)]
 queue = collections.deque()
 visit = [[False] * N for _ in range(N)]
 
-queue.append((0, 0))
-visit[0][0] = True
 
+def queue_push(i, j):
+    if i < N and j < N and not visit[i][j]:
+        queue.append((i, j))
+        visit[i][j] = True
+
+
+queue_push(0, 0)
 while queue:
     i, j = queue.popleft()
     if i == j == N - 1:
         print("HaruHaru")
         break
-
-    step = board[i][j]
-    if i + step < N and not visit[i + step][j]:  # move down
-        visit[i + step][j] = True
-        queue.append((i + step, j))
-    if j + step < N and not visit[i][j + step]:  # move right
-        visit[i][j + step] = True
-        queue.append((i, j + step))
+    queue_push(i + board[i][j], j)
+    queue_push(i, j + board[i][j])
 else:
     print("Hing")
