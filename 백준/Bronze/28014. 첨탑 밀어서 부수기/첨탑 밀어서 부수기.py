72c42e7413e848d482831d01c9804c3777718d86
YongHwan Song <106457455+yonghwna@users.noreply.github.com>
2023-05-19 16:20:24 +0900

    [Bronze III] Title: 첨탑 밀어서 부수기, Time: 1820 ms, Memory: 621888 KB -BaekjoonHub

---
+++ 백준/Bronze/28014. 첨탑 밀어서 부수기/첨탑 밀어서 부수기.py
@@ -0,0 +1,17 @@
+import sys
+
+input = sys.stdin.readline
+N = int(input())
+heights = tuple(map(int, input().split()))
+count = 0
+p = 0
+
+while p < len(heights):
+    count += 1
+
+    i = p
+    while i < len(heights) - 1 and heights[i] > heights[i + 1]:
+        i += 1
+    p = i + 1
+
+print(count)
