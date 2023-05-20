3213a43a84f9db0c6f7ebe7ccb64e8e3e7431759
YongHwan Song <106457455+yonghwna@users.noreply.github.com>
2023-05-20 15:40:08 +0900

    [Bronze III] Title: 별 찍기 - 7, Time: 116 ms, Memory: 113112 KB -BaekjoonHub

--- 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.py
+++ 백준/Bronze/2444. 별 찍기 － 7/별 찍기 － 7.py
@@ -1,3 +1,3 @@
 N = int(input())
-lines = ["{}{}".format(" " * (N - 1 - i), "*" * (2 * i + 1)) for i in range(N)]
+lines = [f"{' ' * (N - 1 - i)}{'*' * (2 * i + 1)}" for i in range(N)]
 print(*(lines + lines[-2::-1]), sep="\n")
