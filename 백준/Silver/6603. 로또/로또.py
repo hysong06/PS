db0e297e7e8138a7340163cf5cff02a55e7590a0
YongHwan Song <106457455+yonghwna@users.noreply.github.com>
2023-06-01 13:20:23 +0900

    [Silver II] Title: 로또, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/6603. 로또/로또.py
+++ 백준/Silver/6603. 로또/로또.py
@@ -1,17 +1,17 @@
 import sys
 
 while True:
-    k, *S = map(int, sys.stdin.readline().split())
-    if k == 0:
+    k, *S = sys.stdin.readline().split()
+    if k == "0":
         break
     stack = []
 
     def solve(depth, cur):
         if depth == 6:
-            print(*stack)
+            print(" ".join(stack))
             return
 
-        for nxt in range(cur + 1, k - (6 - depth) + 1):
+        for nxt in range(cur + 1, len(S) - (6 - depth) + 1):
             stack.append(S[nxt])
             solve(depth + 1, nxt)
             stack.pop()
