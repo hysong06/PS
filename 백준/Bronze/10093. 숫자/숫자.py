d1b24c961c19ca79b249ec664086e4c7d4fd3298
YongHwan Song <106457455+songyha@users.noreply.github.com>
2023-06-02 18:59:02 +0900

    [Bronze II] Title: 숫자, Time: 84 ms, Memory: 35892 KB -BaekjoonHub

--- 백준/Bronze/10093. 숫자/숫자.py
+++ 백준/Bronze/10093. 숫자/숫자.py
@@ -1,12 +1,8 @@
 import sys
 
 input = sys.stdin.readline
-A, B = map(int, input().split())
-if A == B:
-    print(0)
-    print()
-    exit(0)
+A, B = sorted(map(int, input().split()))
+r = range(A + 1, B)
 
-A, B = min(A, B), max(A, B)
-print(B - A - 1)
-print(*[i for i in range(A + 1, B)])
+print(len(r))
+print(*r)
