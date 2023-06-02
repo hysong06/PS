12543e76c74400598e8b7230d4967f4d8513922b
YongHwan Song <106457455+songyha@users.noreply.github.com>
2023-06-03 08:11:55 +0900

    [Bronze II] Title: Strfry, Time: 112 ms, Memory: 34028 KB -BaekjoonHub

--- 백준/Bronze/11328. Strfry/Strfry.py
+++ 백준/Bronze/11328. Strfry/Strfry.py
@@ -1,7 +1,10 @@
+import collections
 import sys
 
 input = sys.stdin.readline
 
 for _ in range(int(input())):
     a, b = input().split()
-    print("Possible" if sorted(a) == sorted(b) else "Impossible")
+    print(
+        "Possible" if collections.Counter(a) == collections.Counter(b) else "Impossible"
+    )
