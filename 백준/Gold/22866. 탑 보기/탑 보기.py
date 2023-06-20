05a2017fda44b829654067af1254bbf66585e86d
YongHwan Song <106457455+songyha@users.noreply.github.com>
2023-06-20 18:56:44 +0900

    [Gold III] Title: 탑 보기, Time: 344 ms, Memory: 133228 KB -BaekjoonHub

---
+++ 백준/Gold/22866. 탑 보기/탑 보기.py
@@ -0,0 +1,32 @@
+import sys
+
+input = sys.stdin.readline
+N = int(input())
+data = list(enumerate(map(int, input().split()), start=1))
+count = [0] * (N + 1)
+nearest = [float("inf")] * (N + 1)
+
+
+def solution(buildings):
+    visibles = []  # stack
+
+    for idx, height in buildings:
+        while visibles and visibles[-1][1] <= height:
+            visibles.pop()
+
+        count[idx] += len(visibles)
+        if visibles and abs(visibles[-1][0] - idx) <= abs(nearest[idx] - idx):
+            nearest[idx] = visibles[-1][0]
+
+        visibles.append((idx, height))
+
+
+# main
+solution(data[::-1])
+solution(data[:])
+
+for v, n in zip(count[1:], nearest[1:]):
+    if v == 0:
+        print(v)
+    else:
+        print(v, n)
