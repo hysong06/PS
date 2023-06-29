33f667ee6be3c4236d1f716a9379297dc4bf36a0
YongHwan Song <106457455+songyha@users.noreply.github.com>
2023-06-29 10:20:48 +0900

    [Silver III] Title: 시리얼 번호, Time: 76 ms, Memory: 34164 KB -BaekjoonHub

--- 백준/Silver/1431. 시리얼 번호/시리얼 번호.py
+++ 백준/Silver/1431. 시리얼 번호/시리얼 번호.py
@@ -2,35 +2,26 @@ import functools
 import sys
 
 
-def compare(a: str, b: str):
+def compare(a: str, b: str) -> int:
     if a == b:
         return 0
 
     """ condition 1 """
-    if len(a) < len(b):
-        return -1
-    if len(a) > len(b):
-        return 1
+    if len(a) != len(b):
+        return len(a) - len(b)
 
     """ condition 2 """
     x = sum(int(ch) for ch in a if ch.isdigit())
     y = sum(int(ch) for ch in b if ch.isdigit())
-
-    if x < y:
-        return -1
-    if x > y:
-        return 1
+    if x != y:
+        return x - y
 
     """ condition 3 """
-    if a < b:
-        return -1
-    return 1  # if a > b
+    return -1 if a < b else 1
 
 
+# main
 input = sys.stdin.readline
 N = int(input())
-print(
-    "\n".join(
-        sorted([input().rstrip() for _ in range(N)], key=functools.cmp_to_key(compare))
-    )
-)
+serials = [input().rstrip("\n") for _ in range(N)]
+print("\n".join(sorted(serials, key=functools.cmp_to_key(compare))))
