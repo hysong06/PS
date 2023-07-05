b70ddf2f6319879db55965fb18888119667fad49
YongHwan Song <106457455+songyha@users.noreply.github.com>
2023-07-05 12:07:57 +0900

    [Silver I] Title: 소수 사이 수열, Time: 408 ms, Memory: 47332 KB -BaekjoonHub

--- 백준/Silver/3896. 소수 사이 수열/소수 사이 수열.py
+++ 백준/Silver/3896. 소수 사이 수열/소수 사이 수열.py
@@ -1,16 +1,15 @@
 import bisect
 import sys
 
-# the sieve of Eratosthenes
-is_prime = [True] * (1299709 + 1)
+sieve = [True] * (1299709 + 1)
 
-is_prime[0] = is_prime[1] = False
-for i in range(2, len(is_prime)):
-    if is_prime[i]:
-        for k in range(i * i, len(is_prime), i):
-            is_prime[k] = False
+sieve[0] = sieve[1] = False
+for i in range(2, len(sieve)):
+    if sieve[i]:
+        for k in range(i * i, len(sieve), i):
+            sieve[k] = False
 
-sorted_primes = [i for i in range(2, len(is_prime)) if is_prime[i]]
+primes = [i for i in range(2, len(sieve)) if sieve[i]]
 
 
 # main
@@ -19,9 +18,8 @@ input = sys.stdin.readline
 for _ in range(int(input())):
     k = int(input())
 
-    if is_prime[k]:
+    if sieve[k]:
         print(0)
-        continue
-
-    idx = bisect.bisect(sorted_primes, k)
-    print(sorted_primes[idx] - sorted_primes[idx - 1])
+    else:
+        idx = bisect.bisect(primes, k)
+        print(primes[idx] - primes[idx - 1])
