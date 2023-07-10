05f41f45e55a3765baf9fdaad2db0af2ada57151
YongHwan Song <106457455+songyha@users.noreply.github.com>
2023-07-10 15:37:09 +0900

    [Silver II] Title: N과 M (12), Time: 48 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/15666. N과 M （12）/N과 M （12）.py
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.py
@@ -1,11 +1,11 @@
 import itertools
 import sys
 
-input = sys.stdin.readline
-N, M = map(int, input().split())
-array = map(int, input().split())
+_, M, *elements = sys.stdin.read().split()
+M = int(M)
+elements = sorted(set(map(int, elements)))
 
 *itertools.starmap(
     print,
-    sorted(set(itertools.combinations_with_replacement(sorted(array), M))),
+    sorted(set(itertools.combinations_with_replacement(elements, M))),
 ),
