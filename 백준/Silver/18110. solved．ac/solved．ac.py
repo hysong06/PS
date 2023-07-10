6b1bdeb8e6f5aa01ba464cd15527a1d0e7c5dc02
YongHwan Song <106457455+songyha@users.noreply.github.com>
2023-07-10 10:22:50 +0900

    [Silver IV] Title: solved.ac, Time: 128 ms, Memory: 55596 KB -BaekjoonHub

--- 백준/Silver/18110. solved．ac/solved．ac.py
+++ 백준/Silver/18110. solved．ac/solved．ac.py
@@ -1,17 +1,14 @@
 import sys
 
-input = sys.stdin.readline
-n = int(input())
-levels = [int(input()) for _ in range(n)]
+n, *levels = map(int, sys.stdin.read().split())
 
 
 def solution():
     if n == 0:
         return 0
 
-    levels.sort()
     k = int(n * 0.15 + 0.5)
-    return int(sum(levels[k : n - k]) / (n - 2 * k) + 0.5)
+    return int(sum(sorted(levels)[k : n - k]) / (n - 2 * k) + 0.5)
 
 
 print(solution())
