fde8c797adbfc672f4bbd8f884396021b51e3789
YongHwan Song <106457455+songyha@users.noreply.github.com>
2023-07-20 23:32:46 +0900

    [Bronze II] Title: 진법 변환, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/2745. 진법 변환/진법 변환.py
+++ 백준/Bronze/2745. 진법 변환/진법 변환.py
@@ -1,1 +1,15 @@
-print(int((a:=input().split())[0],int(a[1])))
+import sys
+
+input = sys.stdin.readline
+N, B = input().split()
+B = int(B)
+to_value = {ch: v for v, ch in enumerate("0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ")}
+
+result = 0
+unit = B ** (len(N) - 1)
+
+for ch in N:
+    result += to_value[ch] * unit
+    unit //= B
+
+print(result)
