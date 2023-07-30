877def342335c56ea4c44434136f01ef931f1a69
YongHwan Song <106457455+songyha@users.noreply.github.com>
2023-07-30 17:24:41 +0900

    [Silver IV] Title: 수 이어 쓰기 1, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1748. 수 이어 쓰기 1/수 이어 쓰기 1.py
+++ 백준/Silver/1748. 수 이어 쓰기 1/수 이어 쓰기 1.py
@@ -2,13 +2,9 @@ import sys
 
 N = sys.stdin.readline().rstrip()
 N, len_N = int(N), len(N)
-i, len_i = 1, 1
 result = 0
 
-while len_i < len_N:
-    result += len_i * (10 * i - i)
-    i, len_i = i * 10, len_i + 1
-
-result += len_i * (N - i + 1)
+for k in range(len_N):
+    result += N - 10**k + 1
 
 print(result)
