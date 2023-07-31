56acdde86bb6e39653547b251aff151b1cb7bab0
YongHwan Song <106457455+songyha@users.noreply.github.com>
2023-07-31 23:33:14 +0900

    [Silver I] Title: 쉬운 계단 수, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.py
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.py
@@ -1,16 +1,10 @@
 import sys
 
 N = int(sys.stdin.readline())
-dp = [[0] * N for _ in range(10)]
 
 # bottom-up
-for r in range(1, 10):
-    dp[r][0] = 1
+dp = [0, *([1] * 9)]
+for _ in range(N - 1):
+    dp = [dp[1], *(dp[i - 1] + dp[i + 1] for i in range(1, 9)), dp[8]]
 
-for c in range(1, N):
-    dp[0][c] = dp[1][c - 1]
-    dp[9][c] = dp[8][c - 1]
-    for r in range(1, 9):
-        dp[r][c] = dp[r - 1][c - 1] + dp[r + 1][c - 1]
-
-print(sum(dp[r][-1] for r in range(10)) % 1_000_000_000)
+print(sum(dp) % 1_000_000_000)
