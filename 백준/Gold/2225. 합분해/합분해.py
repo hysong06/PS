d429a985e8830679fc83046b82426adf72230cf6
YongHwan Song <106457455+songyha@users.noreply.github.com>
2023-08-03 14:33:04 +0900

    [Gold V] Title: 합분해, Time: 200 ms, Memory: 116988 KB -BaekjoonHub

--- 백준/Gold/2225. 합분해/합분해.py
+++ 백준/Gold/2225. 합분해/합분해.py
@@ -4,13 +4,14 @@ input = sys.stdin.readline
 N, K = map(int, input().split())
 memo = [[0] * (N + 1) for _ in range(K + 1)]
 
-for n in range(N + 1):
-    memo[1][n] = 1
+for i in range(N + 1):
+    memo[1][i] = 1
 
-for k in range(2, K + 1):
-    for n in range(0, N + 1):
-        for m in range(0, n + 1):
-            memo[k][n] += memo[k - 1][n - m]
-            memo[k][n] %= 1_000_000_000
 
-print(memo[K][N])
+def solve(k, n):
+    if memo[k][n] == 0:
+        memo[k][n] = sum(solve(k - 1, n - i) for i in range(n + 1)) % 1_000_000_000
+    return memo[k][n]
+
+
+print(solve(K, N))
