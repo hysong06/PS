455abdf778adf020e7447f2ecd66a9e8539d7252
YongHwan Song <106457455+songyha@users.noreply.github.com>
2023-09-01 23:14:53 +0900

    [Gold III] Title: 말이 되고픈 원숭이, Time: 684 ms, Memory: 150168 KB -BaekjoonHub

---
+++ 백준/Gold/1600. 말이 되고픈 원숭이/말이 되고픈 원숭이.py
@@ -0,0 +1,35 @@
+import collections
+import sys
+
+input = sys.stdin.readline
+K = int(input())
+W, H = map(int, input().split())
+board = [input().split() for _ in range(H)]
+visit = [[[False] * W for _ in range(H)] for _ in range(K + 1)]
+queue = collections.deque([(0, 0, 0, 0)])
+
+
+def monkey_moves(i, j, k):
+    if 0 <= j < H and 0 <= k < W and board[j][k] == "0" and not visit[i][j][k]:
+        queue.append((i, j, k, level + 1))
+        visit[i][j][k] = True
+
+
+while queue:
+    clop, x, y, level = queue.popleft()
+    if x == H - 1 and y == W - 1:
+        print(level)
+        break
+
+    # 1. move normally
+    for dx, dy in ((1, 0), (0, 1), (-1, 0), (0, -1)):
+        monkey_moves(clop, x + dx, y + dy)
+
+    # 2. move like horse
+    if clop == K:
+        continue
+    for dx, dy in ((2, 1), (2, -1), (-2, 1), (-2, -1), (1, 2), (1, -2), (-1, 2), (-1, -2)):
+        monkey_moves(clop + 1, x + dx, y + dy)
+
+else:
+    print(-1)
