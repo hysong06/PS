462a90e245f4cd11caf04aa89fd8d161e6c2bb2e
YongHwan Song <106457455+songyha@users.noreply.github.com>
2023-09-01 23:24:47 +0900

    [Gold III] Title: 말이 되고픈 원숭이, Time: 640 ms, Memory: 158508 KB -BaekjoonHub

--- 백준/Gold/1600. 말이 되고픈 원숭이/말이 되고픈 원숭이.py
+++ 백준/Gold/1600. 말이 되고픈 원숭이/말이 되고픈 원숭이.py
@@ -4,22 +4,20 @@ import sys
 input = sys.stdin.readline
 K = int(input())
 W, H = map(int, input().split())
-board = [input().split() for _ in range(H)]
+obstacle = [[*map(lambda x: x == "1", input().split())] for _ in range(H)]
 visit = [[[False] * W for _ in range(H)] for _ in range(K + 1)]
 queue = collections.deque([(0, 0, 0, 0)])
-
-
-def monkey_moves(i, j, k):
-    if 0 <= j < H and 0 <= k < W and board[j][k] == "0" and not visit[i][j][k]:
-        queue.append((i, j, k, level + 1))
-        visit[i][j][k] = True
-
 
 while queue:
     clop, x, y, level = queue.popleft()
     if x == H - 1 and y == W - 1:
         print(level)
         break
+
+    def monkey_moves(i, j, k):
+        if 0 <= j < H and 0 <= k < W and not obstacle[j][k] and not visit[i][j][k]:
+            queue.append((i, j, k, level + 1))
+            visit[i][j][k] = True
 
     # 1. move normally
     for dx, dy in ((1, 0), (0, 1), (-1, 0), (0, -1)):
