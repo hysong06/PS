59443dae2e9b04f068bd23e4c2c91fcaf1956954
YongHwan Song <106457455+hysong06@users.noreply.github.com>
2023-09-06 21:35:10 +0900

    [Gold IV] Title: 카드 정렬하기, Time: 180 ms, Memory: 34108 KB -BaekjoonHub

--- 백준/Gold/1715. 카드 정렬하기/카드 정렬하기.py
+++ 백준/Gold/1715. 카드 정렬하기/카드 정렬하기.py
@@ -4,11 +4,11 @@ import heapq
 input = sys.stdin.readline
 heap = [int(input()) for _ in range(int(input()))]
 heapq.heapify(heap)
+compare = 0
 
-count = 0
 while len(heap) > 1:
-    num1 = heapq.heappop(heap)
-    num2 = heapq.heappop(heap)
-    count += num1 + num2
-    heapq.heappush(heap, num1 + num2)
-print(count)
+    merged = heapq.heappop(heap) + heapq.heappop(heap)
+    compare += merged
+    heapq.heappush(heap, merged)
+
+print(compare)
