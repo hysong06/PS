02f421a88880db667206bb2d3a846b20e19d8a01
YongHwan Song <106457455+hysong06@users.noreply.github.com>
2023-12-01 08:21:09 +0900

    [Gold III] Title: 텀 프로젝트, Time: 2664 ms, Memory: 394104 KB -BaekjoonHub

--- 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.py
+++ 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.py
@@ -1,33 +1,24 @@
 import sys
 
+sys.setrecursionlimit(10**5)
 input = sys.stdin.readline
 
 for _ in range(int(input())):
     n = int(input())
-    choice = {k: v for k, v in enumerate(map(int, input().split()), start=1)}
+    prefer = {s: t for s, t in enumerate(map(int, input().split()), start=1)}
     left = n
 
-    # one-person teams
-    for k, v in list(choice.items()):
-        if k == v:
-            left -= 1
-            del choice[k]
+    # dfs
+    def find_cycle(student, order):
+        if student not in prefer:  # if already checked
+            return 0
+        choice = prefer[student]
+        del prefer[student]  # check
 
-    # and the others
-    stack = [(i, {i: 1}) for i in choice]
-
-    while stack:
-        cur, order = stack.pop()
-        if cur not in choice:
-            continue
-
-        nxt = choice[cur]
-        del choice[cur]  # checked
-
-        if nxt in order:
-            left -= len(order) + 1 - order[nxt]  # get the size of cycle
+        if choice in order:
+            return len(order) + 1 - order[choice]  # return the size of cycle
         else:
-            order[nxt] = len(order) + 1
-            stack.append((nxt, order))
+            order[choice] = len(order) + 1
+            return find_cycle(choice, order)
 
-    print(left)
+    print(left - sum(find_cycle(i, {i: 1}) for i in range(1, n + 1)))
