652064fecf5183d3d5d9ba6b90b316da56772ed4
YongHwan Song <106457455+hysong06@users.noreply.github.com>
2024-01-02 14:01:17 +0900

    [Gold IV] Title: What's your ETA?, Time: 3328 ms, Memory: 574292 KB -BaekjoonHub

--- 백준/Gold/30974. What's your ETA？/What's your ETA？.py
+++ 백준/Gold/30974. What's your ETA？/What's your ETA？.py
@@ -3,24 +3,22 @@ import heapq
 from collections import defaultdict
 
 # the sieve of Eratosthenes
-is_prime = [False, False] + [True] * 9_999_999
-for i in range(2, int(len(is_prime) ** 0.5) + 1):
-    if is_prime[i]:
-        for k in range(i * i, len(is_prime), i):
-            is_prime[k] = False
+prime = [False, False] + [True] * 9_999_999
+for i in range(2, int(len(prime) ** 0.5) + 1):
+    if prime[i]:
+        for k in range(i * i, len(prime), i):
+            prime[k] = False
 
 # init
 input = sys.stdin.readline
 N, M = map(int, input().split())
 code = [0, *map(int, input().split())]
-graph = [defaultdict(lambda: float("inf")) for _ in range(N + 1)]
+graph = defaultdict(lambda: defaultdict(int))
 
 for _ in range(M):
     u, v, w = map(int, input().split())
-    if not is_prime[code[u] + code[v]]:
-        continue
-    w = min(graph[u][v], w)
-    graph[u][v] = graph[v][u] = w
+    if prime[code[u] + code[v]] and (not graph[u][v] or w < graph[u][v]):
+        graph[u][v] = graph[v][u] = w
 
 # Dijkstra's algorithm
 heap = [(0, 1)]
