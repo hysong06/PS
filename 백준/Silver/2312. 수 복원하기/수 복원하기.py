10f969ec1301b6f2e7acd3c153b257ae2daafe1e
YongHwan Song <106457455+hysong06@users.noreply.github.com>
2024-01-02 11:20:51 +0900

    [Silver III] Title: 수 복원하기, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2312. 수 복원하기/수 복원하기.py
+++ 백준/Silver/2312. 수 복원하기/수 복원하기.py
@@ -5,7 +5,7 @@ input = sys.stdin.readline
 for _ in range(int(input())):
     num = int(input())
 
-    for base in range(2, num + 1):
+    for base in range(2, int(num**0.5) + 1):
         exp = 0
         while num % base == 0:
             num //= base
@@ -13,3 +13,7 @@ for _ in range(int(input())):
 
         if exp:
             print(base, exp)
+
+    # if num is prime itself
+    if num > 1:
+        print(num, 1)
